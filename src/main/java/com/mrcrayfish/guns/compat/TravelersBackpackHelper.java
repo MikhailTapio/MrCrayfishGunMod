869fe6815267b45748bf2d92cffe213b99c3ba4a
北斗余晖 <81358657+MikhailTapio@users.noreply.github.com>
2025-09-28 20:57:42 +1000

    :wrench: Fix traveler's backpack compat

--- src/main/java/com/mrcrayfish/guns/compat/TravelersBackpackHelper.java
+++ src/main/java/com/mrcrayfish/guns/compat/TravelersBackpackHelper.java
@@ -3,6 +3,7 @@ package com.mrcrayfish.guns.compat;
 import com.mrcrayfish.guns.common.AmmoContext;
 import com.mrcrayfish.guns.common.Gun;
 import com.tiviacz.travelersbackpack.capability.CapabilityUtils;
+import com.tiviacz.travelersbackpack.inventory.BackpackWrapper;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
@@ -13,19 +14,17 @@ import java.util.concurrent.atomic.AtomicReference;
 public class TravelersBackpackHelper {
     public static AmmoContext findAmmo(Player player, ResourceLocation id)
     {
-        final AtomicReference<AmmoContext> ctx = new AtomicReference<>(AmmoContext.NONE);
-        CapabilityUtils.getCapability(player).ifPresent(t -> {
-            final IItemHandlerModifiable handler = t.getWrapper().inventory;
-            final int size = handler.getSlots();
-            for (int i = 0; i < size; i++)
-            {
-                final ItemStack stack = handler.getStackInSlot(i);
-                if(!Gun.isAmmo(stack, id)) continue;
-                int finalI = i;
-                ctx.set(new AmmoContext(stack, s -> t.getWrapper().setSlotChanged(finalI, s, 0)));
-                return;
-            }
-        });
-        return ctx.get();
+        final BackpackWrapper wrapper = CapabilityUtils.getBackpackWrapper(player);
+        if (wrapper == null) return AmmoContext.NONE;
+        final IItemHandlerModifiable handler = wrapper.inventory;
+        final int size = handler.getSlots();
+        for (int i = 0; i < size; i++)
+        {
+            final ItemStack stack = handler.getStackInSlot(i);
+            if(!Gun.isAmmo(stack, id)) continue;
+            int finalI = i;
+            return new AmmoContext(stack, s -> wrapper.setSlotChanged(finalI, s, 0));
+        }
+        return AmmoContext.NONE;
     }
 }
